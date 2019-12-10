aabcacfb48e20efcebc7c330be9bf8f02a2a4e30
qipng nie <qiping.nie@upai.com>
2019-12-10 18:39:26 +0800

    assign standard

--- copy_and_move_file/move_copy_file.py
+++ copy_and_move_file/move_copy_file.py
@@ -7,7 +7,7 @@ from base64 import b64encode
 from urllib.parse import quote, unquote
 
 
-class QuertUpyun(object):
+class QueryUpyun(object):
 	def __init__(self, bucket, username, password):
 		self.bucket = bucket
 		self.username = username
@@ -15,7 +15,7 @@ class QuertUpyun(object):
 		self.upyun_api = 'http://v0.api.upyun.com'
 		
 
-	def _auth(self):
+	def _Auth(self):
 		req_headers = {
 			"Authorization": "Basic " + b64encode((self.username + ':' + self.password).encode()).decode(),
 			'User-Agent':'Auth-Nie-Python'
@@ -23,40 +23,41 @@ class QuertUpyun(object):
 		}
 		return req_headers
 
-	def reqMoveFile(self, move_source, move_destination):
-		headers = deepcopy(self._auth())
+	def req_move_file(self, move_source, move_destination):
+		headers = deepcopy(self._Auth())
 		headers['X-Upyun-Move-Source'] = ('/' + self.bucket + move_source).encode('utf-8').decode('latin1')
 		s = requests.Session()
 		key = '/' + self.bucket  + move_destination + move_source
 
-		respMove = s.put(self.upyun_api + key, headers=headers)
+		resp_move = s.put(self.upyun_api + key, headers=headers)
 		print("Move " + move_source + " to " + move_destination + move_source)
 
-		if respMove.status_code != 200:
-			with open('movePathError.txt', 'a') as f:
+		if resp_move.status_code != 200:
+			with open('move_path_error.txt', 'a') as f:
 				f.write(move_source + '\n')
 
-	def reqCopyFile(self, copy_source, copy_destination):
-		headers = deepcopy(self._auth())
+	def req_copy_file(self, copy_source, copy_destination):
+		headers = deepcopy(self._Auth())
 		headers['X-Upyun-Copy-Source'] = ('/' + self.bucket + copy_source).encode('utf-8').decode('latin1')
 		s = requests.Session()
 		key = '/' + self.bucket  + copy_destination + copy_source
 
-		respCopy = s.put(self.upyun_api + key, headers=headers)
-		print("Copy " + copy_source + ' To ' + copy_destination + copy_source)
+		resp_copy = s.put(self.upyun_api + key, headers=headers)
+		print("Copy " + copy_source + ' to ' + copy_destination + copy_source)
 
-		if respCopy.status_code != 200:
-			with open('copyPathError.txt', 'a') as f:
+		if resp_copy.status_code != 200:
+			with open('copy_path_error.txt', 'a') as f:
 				f.write(copy_source +  '\n')
+				print(copy_source)
 
 
 
 if __name__ == '__main__':
-	copyAndMoveInit = QuertUpyun('BUCKET_NAME', 'OperatorName', 'OperatorPass')
-	for path in open('LocalListFilePath'):
+	copy_and_move_init = QueryUpyun('BUCKETNAME', 'OPERATOR', 'OPERATOR_PASSWORD')
+	for path in open('LOCAL_FILE_PATH'):
 		path = (path.rstrip())
 		
-		# copyAndMoveInit.reqCopyFile(path, '/COPYPATH')
-		copyAndMoveInit.reqMoveFile(path, '/MOVEPATH')
+		# copy_and_move_init.req_copy_file(path, '/COPYPATH')
+		# copy_and_move_init.req_move_file(path, '/MOVEPATH')
 	else:
 		print("Job is done!")
